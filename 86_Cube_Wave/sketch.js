18e86a6894cbca73ac1d369888f7a6e177af9273
b-sirius <padfoot_07@163.com>
2018-03-13 20:38:41 +0800

    colorful box!!!

--- 86_Cube_Wave/sketch.js
+++ 86_Cube_Wave/sketch.js
@@ -1,6 +1,5 @@
 let angle = 0;
 let w = 20;
-let magicAngle = Math.atan(1 / Math.sqrt(2));
 let maxD = dist(0, 0, 200, 200)
 
 let boxWidth = 300;
@@ -15,33 +14,86 @@ function setup() {
 }
 
 function draw() {
-    background(255, 214, 193);
+    background(255, 255, 255);
     ortho(300, -300, -300, 300, 0, 1000);
 
-    directionalLight(200, 0, 200, 0.6, -0.3, -0.7);
-    ambientLight(200, 0, 200);
+    ambientLight(255, 255, 255);
     noStroke();
     
-    rotateX(-PI / 4);
-    rotateY(PI / 4);
+    rotateX(- PI / 4);
+    rotateY(- PI / 4);
     
     for (let z = 0; z < boxWidth; z += w) {
         for (let x = 0; x < boxHeight; x += w) {
             push();
             let d = dist(x, z,  boxWidth / 2, boxHeight / 2);
-            let offset = map(d, 0, maxD, -4, 4);
+            let offset = map(d, 0, maxD, -5, 5);
 
             let a = angle + offset;
     
-            let h = map(sin(a), -1, 1, 0, 180) + 120;
+            let h = map(sin(a), -1, 1, 0, 190) + 110;
     
             translate(x - boxWidth / 2, 0, z - boxWidth / 2);
             ambientMaterial(231, 0, 62);
-            box(w, h, w);
+            colorBox(w, h, w, [223, 216, 157], [115, 167, 160], [59, 78, 135]);
             
             pop();
         }
     }
 
-    angle -= 0.1;
+    angle -= 0.07;
+
+    // colorBox(50, 150, 20, [252, 251, 214], [113, 111, 237], [111, 230, 237])
+}
+
+function colorBox(xLength, yLength, zLength, color1, color2, color3) {
+    // 前后面
+    push();
+    ambientMaterial(...color1);
+    
+    push();
+    translate(0, 0, - zLength / 2);
+    plane(xLength, yLength);
+    pop();
+
+    push();
+    translate(0, 0, zLength / 2);
+    plane(xLength, yLength);
+    pop();
+
+    pop();
+
+    // 上下面
+    push();
+    ambientMaterial(...color2);
+    rotateX(PI / 2);
+
+    push();
+    translate(0, 0, yLength / 2);
+    plane(xLength, zLength);
+    pop();
+
+    push();
+    translate(0, 0, - yLength / 2);
+    plane(xLength, zLength);
+    pop();
+
+    pop();
+    
+    // 左右面
+    push();
+    ambientMaterial(...color3);
+    rotateY(PI / 2);
+    
+    push();
+    translate(0, 0, xLength / 2);
+    plane(zLength, yLength);
+    pop();
+
+    push();
+    translate(0, 0, - xLength / 2);    
+    plane(zLength, yLength);
+    pop();
+
+    pop();
 }
