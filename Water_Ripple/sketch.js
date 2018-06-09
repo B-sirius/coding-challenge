681b1b62f4302a7a434bc57f266ab0af2bf01164
b-sirius <padfoot_07@163.com>
2018-06-09 09:54:24 +0800

    add image

--- Water_Ripple/sketch.js
+++ Water_Ripple/sketch.js
@@ -1,31 +1,38 @@
 'use strict';
 
-const ROWS = 200;
-const COLS = 200;
-let dampening = 0.99;
+const ROWS = 400;
+const COLS = 400;
+let dampening = 1 - 1 / 32;
 
 let previous;
 let current;
+let CrispKnee;
+let imagePixelsCopy;
 
 function getWaterArray(rgb) {
     return Array.from({ length: ROWS }).map(i => Array.from({ length: COLS }).fill(rgb));
 }
 
-function mousePressed() {
-    previous[mouseX][mouseY] = 3000;
+function mouseDragged() {
+    previous[mouseX][mouseY] = 500;
+}
+
+function preload() {
+    CrispKnee = loadImage('./witcher3.jpg');
 }
 
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(400, 400);
     pixelDensity(1);
     previous = getWaterArray(0);
     current = getWaterArray(0);
+
+    CrispKnee.loadPixels();
+    imagePixelsCopy = Array.from(CrispKnee.pixels);
 }
 
 function draw() {
-    background(0);
-
-    loadPixels();
+    CrispKnee.loadPixels();
     // 循环非边缘的对象
     for (let i = 1; i < COLS - 1; i++) {
         for (let j = 1; j < ROWS - 1; j++) {
@@ -38,15 +45,32 @@ function draw() {
 
             current[i][j] *= dampening;
 
-            let index = (i + j * COLS) * 4;
-            pixels[index] = current[i][j];
-            pixels[index + 1] = current[i][j];
-            pixels[index + 2] = current[i][j];
+            const data = 1024 - current[i][j];
+
+            // 获得偏移值
+            let xoffset = int((i) * data / 1024);
+            let yoffset = int((j) * data / 1024);
+
+            // 边缘检查
+            if (xoffset >= COLS) xoffset = COLS - 1;
+            else if (xoffset < 0) xoffset = 0;
+            if (yoffset >= ROWS) yoffset = ROWS - 1;
+            else if (yoffset < 0) yoffset = 0;
+
+            // 根据偏移值获得偏移下标
+            const index = (i + j * COLS) * 4;
+            const newIndex = (xoffset + yoffset * COLS) * 4;
+
+            CrispKnee.pixels[index] = imagePixelsCopy[newIndex];
+            CrispKnee.pixels[index + 1] = imagePixelsCopy[newIndex + 1];
+            CrispKnee.pixels[index + 2] = imagePixelsCopy[newIndex + 2];
         }
     }
-    updatePixels();
+    CrispKnee.updatePixels();
 
     let temp = previous;
     previous = current;
     current = temp;
+
+    image(CrispKnee, 0, 0);
 }
